ea17a1979c64fdb93b95efab46fd95e3d3fbb2ac
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-12-16 18:24:17 +0200

    Create Validators form for registration User
      - add validationSchemes.js
      - add validator.js

---
+++ client/src/validators/validationSchemes.js
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+export default {
+    LoginSchema: yup.object().shape({
+        email: yup.string().email('check email').required('Email is required'),
+        password: yup.string().test('test-password','min 6 symbols',value => (value && value.trim().length>=6)).required('Password is required')
+    }),
+    RegistrationSchema: yup.object().shape({
+        email: yup.string().email('check email').required('Email is required'),
+        password: yup.string().test('test-password','min 6 symbols',value => (value && value.trim().length>=6)).required('Password is required'),
+        firstName: yup.string().test('test-firstName','required',value => (value && value.trim().length>=1)).required('First Name is required'),
+        lastName: yup.string().test('test-lastName','required',value => (value && value.trim().length>=1)).required('Last Name is required')
+    })
+}
