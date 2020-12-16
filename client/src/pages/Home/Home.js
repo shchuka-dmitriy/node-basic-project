fbc8bb3a2ed472e7654e5edb9f34c94bf80d8bcc
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-12-16 18:18:22 +0200

    Create Home page
      - add Home.js
      - add Home.module.sass

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styles from "./Home.module.sass"
+import Header from "../../components/Header/Header";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+};
+
+export default Home;
