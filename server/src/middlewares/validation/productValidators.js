77df9c8a86717c232fc12b6e8cbffdabb36b8607
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:30:27 +0200

    Change product validators on server part
      - edit productValidators.js

--- server/src/middlewares/validation/productValidators.js
+++ server/src/middlewares/validation/productValidators.js
@@ -2,10 +2,9 @@ const schemas = require('../../utils/validation/product');
 const { BadRequestError } = require("../../utils/errors");
 
 module.exports.productCreationValidate = async (req, res, next) => {
-    const { productName, productType, attributes } = req.body;
-    const productValidationResult = await schemas.productSchema.isValid( {productName, productType} );
-    const attributesValidationResult = await schemas.attributesSchema.isValid( attributes );
-    if (productValidationResult && attributesValidationResult) {
+    const { productName, productType, weight, color, price } = req.body;
+    const productValidationResult = await schemas.productSchema.isValid( {productName, productType, weight, color, price} );
+    if (productValidationResult) {
         next();
     } else {
         next( new BadRequestError('Invalid data for create product') );
