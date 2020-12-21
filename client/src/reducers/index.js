7f1eefd2e081a45842ea896dd265315b1674d4c3
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:23:57 +0200

    Change index reducer
      - edit reducers/index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,20 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import authReducer from "./authReducer";
+import getProductsReducer from "./getProductsReducer";
+import getProductByIdReducer from "./getProductByIdReducer";
+import updateProductReducer from "./updateProductReducer";
+import createProductReducer from "./createProductReducer";
+import getUserReducer from './userReducer';
 
 const appReducer = combineReducers({
     form: formReducer,
     auth: authReducer,
+    userStore: getUserReducer,
+    productStore: createProductReducer,
+    productsStore: getProductsReducer,
+    selectProductStore: getProductByIdReducer,
+    updatedProductStore: updateProductReducer,
 });
 
 export default appReducer;
