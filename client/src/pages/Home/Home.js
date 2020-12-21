ab6ade1c0dc29eb7f4a1502f70ae1021017d1c9b
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:03:16 +0200

    Change Home page
      - change Home.js
      - change Home.module.sass

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import styles from "./Home.module.sass"
+import styles from "./Home.module.sass"
 import Header from "../../components/Header/Header";
+import ProductsList from "../../components/ProductsList/ProductsList";
+import history from "../../browserHistory";
 
 const Home = () => {
+    const goToCreateProduct = () => {
+        history.push('/createProduct');
+    };
 
     return (
-        <div>
+        <div className={styles.mainContainer}>
             <Header/>
+            <ProductsList/>
+            <button onClick={goToCreateProduct} className={styles.createButton}>
+                create new product
+            </button>
         </div>
     )
 };
