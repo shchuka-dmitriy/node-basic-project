9282605c95587b8b874a1857d4089c8cc3e4eb7a
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:27:35 +0200

    Change main App.js and index.css on client part
      - edit App.js
      - edit index.css

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,27 @@ import Home from './pages/Home/Home';
 import browserHistory from './browserHistory';
 import './App.css';
 import './reset.css';
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import NotFound from "./components/NotFound/NotFound";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import CreateProductPage from "./pages/CreateProductPage/CreateProductPage.js";
 
 class App extends Component {
-  render() {
-    return (
-        <Router history={browserHistory}>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/registration' component={RegistrationPage}/>
-            <Route exact path='/login' component={LoginPage}/>
-          </Switch>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/login' component={LoginPage}/>
+                    <Route exact path='/registration' component={RegistrationPage}/>
+                    <Route exact path='/product/:id' component={ProductPage}/>
+                    <Route exact path='/createProduct' component={CreateProductPage}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- client/src/constants.js
+++ client/src/constants.js
@@ -2,18 +2,13 @@ const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = env === 'production' ? 3000 : 9634;
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   STATIC_IMAGES_PATH: '/staticImages/',
-  STATIC_PHOTOS_PATH: '/photos/',
-  ANONYM_IMAGE_PATH: '/staticImages/anonymous2.png',
-  ANONYM_PHOTO: 'anon.png',
-  BASE_URL: `http://${ serverIP }:${ serverPort }/`,
+  BASE_URL: `http://${ serverIP }:${ serverPort }/api/v1/`,
   HOME_URL: `http://${ serverIP }:3000`,
-  publicURL: env === 'production'
-    ? `http://${ serverIP }:80/photos/`
-    : `http://${ serverIP }:${ serverPort }/public/photos/`,
-  publicPhotosURL: env === 'production'
-      ? `http://${ serverIP }:80/photos/`
-      : `http://${ serverIP }:${ serverPort }/public/photos/`,
-  ACCESS_TOKEN: 'accessToken'
+  ACCESS_TOKEN: 'accessToken',
+  TABLET: 'tablet',
+  NOTEBOOK: 'notebook',
+  PHONE: 'phone',
 };
