a865389089ff2b418e656ade7023967e2edb6fb0
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:20:35 +0200

    Change main Header component
      - edit Header.js
      - edit Header.module.sass

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,41 @@
-import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import React from 'react';
+import styles from './Header.module.sass';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {headerRequest} from '../../actions/actionCreator';
+import LoginButtons from "../LoginButtons/LoginButtons";
 
-class Header extends Component {
+class Header extends React.Component {
+    componentDidMount() {
+        if (!this.props.data) {
+            this.props.getUser();
+        }
+    }
+
     render() {
+        if (this.props.isFetching) {
+            return null;
+        }
         return (
-            <div>
-
-                <Link to='/registration' style={{textDecoration: 'none'}}>
-                    <button>sign up</button>
-                </Link>
-                <Link to='/login' style={{textDecoration: 'none'}}>
-                    <button>login</button>
-                </Link>
-
+            <div className={styles.headerContainer}>
+                <div className={styles.loginSignUpHeaders}>
+                    <div className={styles.userButtonsContainer}>
+                        <LoginButtons data={this.props.data}/>
+                    </div>
+                </div>
             </div>
-        );
+        )
     }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+    return state.userStore;
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getUser: () => dispatch(headerRequest()),
+    }
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
