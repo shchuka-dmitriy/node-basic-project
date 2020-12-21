5b05ae6fb6c5f4680368c1f87d75ff5926db6bf5
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:25:00 +0200

    Create saga for GET user
      - add sagas/userSaga.js

---
+++ client/src/sagas/userSaga.js
@@ -0,0 +1,14 @@
+import {put} from 'redux-saga/effects';
+import ACTION from '../actions/actionTypes';
+import * as restController from '../api/rest/restController';
+
+export function* headerRequest(){
+    yield put({type: ACTION.GET_USER_REQUEST});
+    try{
+        const {data} = yield restController.getUser();
+        yield put({type: ACTION.GET_USER_SUCCESS, data: data});
+    }
+    catch (e) {
+        yield put({type: ACTION.GET_USER_ERROR, error: e.response});
+    }
+}
