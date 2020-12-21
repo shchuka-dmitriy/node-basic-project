cef595e9b7e24bac105bed009d5ef1fc4fb11cf5
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:03:46 +0200

    Change Login and Registration page
      - change LoginPage.js
      - change RegistrationPage.js

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,48 +1,15 @@
 import React from 'react';
-import styles from '../../assets/styles/LoginRegistrationPages.module.sass';
-import {Link} from "react-router-dom";
-import {connect} from 'react-redux';
-import {clearErrorSignUpAndLogin} from '../../actions/actionCreator';
-import PropTypes from 'prop-types';
-import UserForm from '../../components/UserForm/UserForm';
+import LoginAuthMainBlock from "../../components/LoginAuthMainBlock/LoginAuthMainBlock";
 
-const LoginPage = (props) => {
-    props.clearError();
-
+const LoginPage = () => {
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.wrapperContainer}>
-                <div className={styles.headerPage}>
-                    <div className={styles.linkLoginContainer}>
-                        <Link to={'/'} style={{textDecoration: 'none'}}>
-                            <span>To list</span>
-                        </Link>
-                    </div>
-                    <div className={styles.linkLoginContainer}>
-                        <Link to={'/registration'} style={{textDecoration: 'none'}}>
-                            <span>Sign up</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.headerFormContainer}>
-                    <h2 className={styles.formHeader}>
-                        LOGIN TO YOUR ACCOUNT
-                    </h2>
-                    <UserForm />
-                </div>
-            </div>
-        </div>
+        <LoginAuthMainBlock
+            isRegistrationPage={false}
+            logo={'LOGIN TO YOUR ACCOUNT'}
+            link={'/registration'}
+            buttonLabel={'Sign up'}
+        />
     )
 };
 
-LoginPage.propTypes = {
-    clearError: PropTypes.func
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        clearError: () => dispatch(clearErrorSignUpAndLogin())
-    }
-};
-
-export default connect(null, mapDispatchToProps)(LoginPage);
+export default LoginPage;
--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -1,48 +1,15 @@
 import React from 'react';
-import styles from '../../assets/styles/LoginRegistrationPages.module.sass';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {clearErrorSignUpAndLogin} from '../../actions/actionCreator';
-import PropTypes from "prop-types";
-import UserForm from "../../components/UserForm/UserForm";
+import LoginAuthMainBlock from "../../components/LoginAuthMainBlock/LoginAuthMainBlock";
 
-const RegistrationPage = (props) => {
-    props.clearError();
-
+const RegistrationPage = () => {
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.wrapperContainer}>
-                <div className={styles.headerPage}>
-                    <div className={styles.linkLoginContainer}>
-                        <Link to={'/'} style={{textDecoration: 'none'}}>
-                            <span>To list</span>
-                        </Link>
-                    </div>
-                    <div className={styles.linkLoginContainer}>
-                        <Link to='/login' style={{textDecoration: 'none'}}>
-                            <span>Login</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.headerFormContainer}>
-                    <h2 className={styles.formHeader}>
-                        CREATE AN ACCOUNT
-                    </h2>
-                    <UserForm />
-                </div>
-            </div>
-        </div>
+        <LoginAuthMainBlock
+            isRegistrationPage={true}
+            logo={'CREATE AN ACCOUNT'}
+            link={'/login'}
+            buttonLabel={'Login'}
+        />
     )
 };
 
-RegistrationPage.propTypes = {
-    clearError: PropTypes.func
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        clearError: () => dispatch(clearErrorSignUpAndLogin())
-    }
-};
-
-export default connect(null, mapDispatchToProps)(RegistrationPage);
+export default RegistrationPage;
