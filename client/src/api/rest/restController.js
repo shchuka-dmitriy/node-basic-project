ca7d931bf9107e9f9074b7428fc5615c9f08c55e
Illia-S-dev <illia.sobko.dev@gmail.com>
2020-12-21 17:02:12 +0200

    Add requests for CRUD products
      - change api/rest/restController.js.js

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,4 +1,10 @@
 import http from '../interceptor';
 
 export const registerRequest = (data) => http.post('registration', data);
-
+export const loginRequest = (data) => http.post('login', data);
+export const getUser = () => http.get('user');
+export const createProduct = (data) => http.post('product', data);
+export const updateProduct= (data, id) => http.patch(`product/${id}`, data);
+export const deleteProduct = (id) => http.delete(`product/${id}`);
+export const getProducts = (data) => http.post('products', data);
+export const getProductById = (id) => http.get(`product/${id}`);
